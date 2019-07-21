7df18af89ba2224002201dcd6fac52808e1b03ec
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-21 19:28:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,8 @@ class Application
     
     if req.path.match(/items/)
       item_name= req.path.split("/items/").last 
-      if item= @@items.find {|i| 
+      if item= @@items.find {|i| i.name= item_name}
+        
   end
   
 end
