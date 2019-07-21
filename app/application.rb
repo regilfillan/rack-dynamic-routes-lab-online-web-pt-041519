865bc0012a3dd69e5b49b48b77ed8c7cb54f2ee0
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-21 19:31:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,12 +14,11 @@ class Application
         resp.status= 404
         resp.write "Route not found"
       end
-          else
+    else
       resp.status=404
       resp.write "Route not found"
     end
     resp.finish
-  end
   end
   
 end
